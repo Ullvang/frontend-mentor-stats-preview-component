c09d82a24f1ba8fff43e655065c2edb04db46d8e
Jon Erik <jon.erik.ullvang@gmail.com>
2021-09-19 13:31:53 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
 import "./App.css";
+import imageHeaderMobile from "./images/image-header-mobile.jpg";
 
 function App() {
   return (
-    <div className="App">
-      Get insights that help your business grow. Discover the benefits of data
-      analytics and make better decisions regarding revenue, customer
-      experience, and overall efficiency. 10k+ companies 314 templates 12m+
-      queries
-      <div className="attribution">
-        Challenge by{" "}
-        <a
-          href="https://www.frontendmentor.io?ref=challenge"
-          rel="noreferrer"
-          target="_blank"
-        >
-          Frontend Mentor
-        </a>
-        . Coded by <a href="/">Your Name Here</a>.
-      </div>
-    </div>
+    <>
+      <main className="App">
+        <section id="card">
+          {/* <div id="image-contianer">
+            <div id="image-overlay"></div> */}
+          <img src={imageHeaderMobile} alt="people sitting around table" />
+          {/* </div> */}
+          <div id="info-box">
+            <h1>
+              Get <span id="purple">insights</span> that help your business
+              grow.
+            </h1>
+            <p>
+              Discover the benefits of data analytics and make better decisions
+              regarding revenue, customer experience, and overall efficiency.
+            </p>
+            <div id="highlights">
+              <div>
+                <p>
+                  <span>10k+</span> COMPANIES
+                </p>
+              </div>
+              <div>
+                <p>
+                  <span>314</span> TEMPLATES
+                </p>
+              </div>
+              <div>
+                <p>
+                  <span>12M+</span> QUERIES
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer className="attribution">
+        <p>
+          Challenge by{" "}
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Frontend Mentor
+          </a>
+          .
+        </p>
+        <p>
+          Coded by <a href="/">Jon Erik Ullvang</a>.
+        </p>
+      </footer>
+    </>
   );
 }
 
